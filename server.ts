f9666474f4a58b51aab1581436a2b68cf73904c1
Amar Kisoensingh <amar@kisoensingh.sh>
2020-12-21 14:49:05 +0100

    Removed generated React code and added prettier

--- server.ts
+++ server.ts
@@ -3,9 +3,7 @@ import express from 'express'
 const app = express()
 const port = 8000
 
-app.get('/health', (req, res) => {
-  res.sendStatus(200)
-})
+app.get('/health', (req, res) => res.sendStatus(200))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
