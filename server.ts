63f7361770f23ec38dff2e9da3fc8fcda4ea9e69
Amar Kisoensingh <amar@kisoensingh.sh>
2020-12-24 22:39:48 +0100

    Fixed route issue

--- server.ts
+++ server.ts
@@ -25,7 +25,7 @@ const getDefaultDeck = () => {
 
 app.use(express.static(path.join(__dirname, './client')))
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/index.html'))
 })
 
